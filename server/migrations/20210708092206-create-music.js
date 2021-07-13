9e5a5d71c6c7e545783234a37f64ca9e86f933cb
Muhammad Wahyudi <ideapedyudi@gmail.com>
2021-07-13 22:55:52 +0700

    dumbsound

---
+++ server/migrations/20210708092206-create-music.js
@@ -0,0 +1,45 @@
+'use strict';
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    await queryInterface.createTable('music', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      year: {
+        type: Sequelize.STRING
+      },
+      thumbnail: {
+        type: Sequelize.STRING
+      },
+      attache: {
+        type: Sequelize.STRING
+      },
+      idArtis: {
+        type: Sequelize.INTEGER,
+        references: {
+          model: 'artis',
+          key: 'id'
+        },
+        onDelete: "CASCADE",
+        onUpdate: "CASCADE"
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  down: async (queryInterface, Sequelize) => {
+    await queryInterface.dropTable('music');
+  }
+};
